be76a90c1d9f558875a7edebf2ca00f517b279e6
Tharmi1305 <139675003+Tharmi1305@users.noreply.github.com>
2025-03-30 14:18:28 +0530

    Project Initiated

---
+++ myapp_day03/src/main/java/com/example/demo/MyappDay03Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappDay03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappDay03Application.class, args);
+	}
+
+}
