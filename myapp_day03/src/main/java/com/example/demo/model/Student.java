d94a67e310bab62de87a2f3bfe3c5ad93a57a493
Tharmi01 <tharmihansenthur01@gmail.com>
2025-03-31 14:24:11 +0530

    Fix Student model: Add default constructor and getters/setters

--- myapp_day03/src/main/java/com/example/demo/model/Student.java
+++ myapp_day03/src/main/java/com/example/demo/model/Student.java
@@ -15,3 +15,53 @@ public class Student {
 		this.course = course;
 		this.gpa = gpa;
 	}
+
+	public String getRegNo() {
+		return regNo;
+	}
+
+	public void setRegNo(String regNo) {
+		this.regNo = regNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [regNo=" + regNo + ", name=" + name + ", age=" + age + ", course=" + course + ", gpa=" + gpa
+				+ "]";
+	}
+	
+	
+
+}
