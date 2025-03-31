f36cc49e06df54aa969c78094a56a5fa86f6c51d
Tharmi01 <tharmihansenthur01@gmail.com>
2025-03-31 14:21:30 +0530

    properties and constructor added

---
+++ myapp_day03/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+public class Student {
+	private String regNo;
+	private String name;
+	private  int age;
+	private String course;
+	private double gpa;
+	
+	public Student(String regNo,String name,int age,String course,double gpa) {
+		super();
+		this.regNo = regNo;
+		this.name = name;
+		this.age = age;
+		this.course = course;
+		this.gpa = gpa;
+	}
